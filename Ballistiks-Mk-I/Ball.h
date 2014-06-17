72b7a40c31cdb6a98cc24cfc9abac3c03ed5e3d8
planetchili <planetchili@users.noreply.github.com>
2014-06-17 18:03:45 +0900

    add internal drawable class to Ball and GetDrawable()

--- Ballistiks-Mk-I/Ball.h
+++ Ballistiks-Mk-I/Ball.h
@@ -4,8 +4,32 @@
 class Ball : public PhysicalCircle
 {
 public:
+	class Drawable : ::Drawable
+	{
+	public:
+		Drawable( const Ball& parent )
+			:
+			::Drawable( Mat3::Translation( parent.pos ) ),
+			parent( parent )
+		{}
+		virtual void Rasterize( D3DGraphics& gfx ) const override
+		{
+			const float scaledRadius = parent.radius * trans.ExtractScaleIsometric();
+			const Vec2 screenPos = trans * Vec2 { 0.0f,0.0f };
+			gfx.DrawCircleClip( screenPos,(int)scaledRadius,clip,parent.color );
+		}
+	private:
+		const Ball& parent;
+	};
+public:
 	Ball( Vec2 pos )
 		:
 		PhysicalCircle( BALL_RADIUS,BALL_DENSITY,pos,BALL_DRAG )
 	{}
+	Drawable GetDrawable() const
+	{
+		return Drawable( *this );
+	}
+private:
+	const Color color = WHITE;
 };
