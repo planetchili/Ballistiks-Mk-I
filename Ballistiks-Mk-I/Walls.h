85d87e5a02717ff3abb8622e0abf81da593e21e5
planetchili <planetchili@users.noreply.github.com>
2014-06-19 20:45:53 +0900

    walls added

---
+++ Ballistiks-Mk-I/Walls.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "PolyClosed.h"
+#include "TriangleStrip.h"
+
+class Walls
+{
+public:
+	Walls( PolyClosed&& poly )
+		:
+		poly( poly ),
+		strip( this->poly )
+	{}
+	Walls( const PolyClosed& poly )
+		:
+		poly( poly ),
+		strip( poly )
+	{}
+	void HandleCollision( PhysicalCircle& obj )
+	{
+		poly.HandleCollision( obj,PolyClosed::ReboundInternal );
+	}
+	TriangleStrip::Drawable GetDrawable() const
+	{
+		return strip.GetDrawable( color );
+	}
+private:
+	PolyClosed poly;
+	TriangleStrip strip;
+	const Color color;
+};
