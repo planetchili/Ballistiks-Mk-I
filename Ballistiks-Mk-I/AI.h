4f580aa31e9fb4d9c0059f0b32d744236d925364
planetchili <planetchili@users.noreply.github.com>
2014-06-22 11:56:27 +0900

    factory added to ai

--- Ballistiks-Mk-I/AI.h
+++ Ballistiks-Mk-I/AI.h
@@ -1,9 +1,16 @@
 #pragma once
+#include <memory>
 #include "ViewableWorld.h"
 #include "Controller.h"
 
 class AI : public Controller
 {
+public:
+	class Factory
+	{
+	public:
+		virtual std::unique_ptr< AI > Make() const = 0;
+	};
 public:
 	AI( ControllablePlayer& player,const ViewableWorld& view )
 		:
