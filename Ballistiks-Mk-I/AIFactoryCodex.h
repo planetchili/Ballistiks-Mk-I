255ad82317697030e6e896e98ae1ed800fe1cf51
planetchili <planetchili@users.noreply.github.com>
2014-06-23 17:56:01 +0900

    prevent duplicate authors in codex

--- Ballistiks-Mk-I/AIFactoryCodex.h
+++ Ballistiks-Mk-I/AIFactoryCodex.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <memory>
 #include <algorithm>
+#include <assert.h>
 #include "AI.h"
 
 /*************************/
@@ -38,11 +39,29 @@ public:
 			return f->GetAuthor() == author;
 		} );
 	}
+	bool ContainsAuthor( const std::string author ) const
+	{
+
+		return 0 < std::count_if( factories.begin(),factories.end(),
+			[&author]( const std::unique_ptr< AI::Factory >& f ) -> bool
+		{
+			return f->GetAuthor() == author;
+		} );
+	}
 private:
 	template <typename T>
 	void Register()
 	{
-		factories.push_back( std::make_unique< T >() );
+		std::unique_ptr< AI::Factory > pFactory = std::make_unique< T >();
+
+		if( !ContainsAuthor( pFactory->GetAuthor() ) )
+		{
+			factories.push_back( std::move( pFactory ) );
+		}
+		else
+		{
+			assert( false );
+		}
 	}
 private:
 	std::vector< std::unique_ptr< AI::Factory > > factories;
