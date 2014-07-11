1588cfc28c4f6d6ddad66d7e03386869e2b02504
planetchili <planetchili@users.noreply.github.com>
2014-07-11 15:59:24 +0900

    presentator minimally implemented (maybe)

--- Ballistiks-Mk-I/Presentator.cpp
+++ Ballistiks-Mk-I/Presentator.cpp
@@ -5,3 +5,20 @@ void Presentator::BeginTitleState::OnCountdownFinished()
 {
 	parent.Transition( std::make_unique< Presentator::GameOn >( parent ) );
 }*/
+
+void Presentator::GameOn::OnGoal()
+{
+	parent.manager->StartNewPoint();
+	parent.Transition( std::make_unique< GameOn >( parent ) );
+}
+
+void Presentator::GameOn::OnPeriod()
+{
+	parent.manager->StartNewPeriod();
+	parent.Transition( std::make_unique< GameOn >( parent ) );
+}
+
+void Presentator::GameOn::Draw( DrawTarget& tgt )
+{
+	parent.manager->RenderWorld( tgt );
+}
--- Ballistiks-Mk-I/Presentator.h
+++ Ballistiks-Mk-I/Presentator.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <memory>
+#include "GameManager.h"
 #include "DrawTarget.h"
+#include "Midi.h"
+#include "Sound.h"
 
 class Presentator
 {
+#pragma region States
 private:
 	class State
 	{
@@ -15,7 +19,7 @@ private:
 		virtual void Step( float dt ) {}
 		virtual void OnGoal() {}
 		virtual void OnPeriod() {}
-		virtual void Draw( DrawTarget& dt ) {}
+		virtual void Draw( DrawTarget& tgt ) {}
 	protected:
 		Presentator& parent;
 	};
@@ -46,21 +50,87 @@ private:
 		virtual void OnCountdownFinished() override;
 		virtual void Draw( DrawTarget& dt );
 	};
+	class NullState : public State
+	{
+	public:
+		NullState( Presentator& parent )
+			:
+			State( parent )
+		{}
+	};
 	class GameOn : public State
 	{
 	public:
 		GameOn( Presentator& parent )
 			:
 			State( parent )
-		{}
+		{
+			parent.whistleShort.Play();
+		}
 		virtual void OnGoal() override;
 		virtual void OnPeriod() override;
-		virtual void Draw( DrawTarget& dt ) {}
+		virtual void Draw( DrawTarget& tgt ) override;
 	};
-public:
-	inline State& GetState()
+#pragma endregion
+#pragma region Observers
+private:
+	class PointObserver : public Observer
 	{
-		return *state;
+	public:
+		virtual void OnNotify() override
+		{
+			parent.state->OnGoal();
+		}
+		static std::shared_ptr< PointObserver > Make( Presentator& parent )
+		{
+			return std::shared_ptr< PointObserver >( new PointObserver( parent ) );
+		}
+	private:
+		PointObserver( Presentator& parent )
+			:
+			parent( parent )
+		{}
+	private:
+		Presentator& parent;
+	};
+	class PeriodObserver : public Observer
+	{
+	public:
+		virtual void OnNotify() override
+		{
+			parent.state->OnPeriod();
+		}
+	private:
+		PeriodObserver( Presentator& parent )
+			:
+			parent( parent )
+		{}
+	private:
+		Presentator& parent;
+	};
+#pragma endregion
+public:
+	Presentator( DSound& sound )
+		:
+		whistleShort( sound.CreateSound( "whistle.wav" ) ),
+		state(std::make_unique< NullState >( *this ))
+	{}
+	inline void Step( float dt )
+	{
+		state->Step( dt );
+	}
+	inline void Draw( DrawTarget& tgt )
+	{
+		state->Draw( tgt );
+	}
+	inline const GameManager& GetManager() const
+	{
+		return *manager;
+	}
+	inline void StartGame( Controller::Factory& teamA,Controller::Factory& teamB )
+	{
+		manager = std::make_unique< GameManager >( teamA,teamB );
+		Transition( std::make_unique<GameOn>( *this ) );
 	}
 private:
 	inline void Transition( std::unique_ptr< State > newState )
@@ -68,5 +138,13 @@ private:
 		state = std::move( newState );
 	}
 private:
+	//MidiSong batmanTheme;
+	//MidiSong littleHobo;
+	Sound whistleShort;
+	//Sound whistleLong;
+	//Sound batmanTransition;
 	std::unique_ptr< State > state;
+	std::unique_ptr< GameManager > manager;
+	std::shared_ptr< PointObserver > ptObs;
+	std::shared_ptr< PeriodObserver > perObs;
 };
